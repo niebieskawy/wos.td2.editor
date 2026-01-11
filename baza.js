7c837cfb3a0d2db32410a7ae1e4a638f46131418
niebieskawy <chudini.info@gmail.com>
2026-01-11 02:57:56 +0100

    Create baza.js

---
+++ baza.js
@@ -0,0 +1,258 @@
+const BAZA_OSTRZEZEN = {
+"Arkadia Zdrój": [
+    ["0.563", "0.585", "zły stan techniczny wiaduktu", "4", "70", "70", "* ) doraźne"],
+    ["30.318", "30.335", "zły stan techniczny wiaduktu", "1,2", "70", "70", "* ) doraźne"]
+  ],
+  "Berety Kamielskie": [
+    ["40.635", "41.302", "Prace torowe na sąsiednim torze", "6", "20", "20", ""]
+  ],
+  "Brzezinka Średzka": [
+    ["13.800", "15.100", "zły stan toru", "2", "40", "40", "273"],
+    ["19.960", "20.195", "zły stan toru", "1", "70", "70", ""],
+    ["20.620", "21.400", "zły stan toru", "4", "20", "20", ""],
+    ["21.480", "21.513", "zły stan toru", "2", "100", "100", ""],
+    ["21.530", "21.890", "zły stan toru", "1", "70", "70", ""],
+    ["22.486", "22.926", "brak widoczności na przejeździe w km 22.926", "1", "70", "-", ""],
+    ["22.926", "23.296", "", "1", "-", "60", ""],
+    ["22.926", "23.926", "", "2", "-", "50", ""]
+  ],
+  "Chełmik Wołowski": [
+    ["39.430", "39.500", "Zły stan przejścia rozjazdowego", "-", "20", "20", "dotyczy przejścia 5-6, jazda zwrotna"]
+  ],
+  "Dąbrówno": [
+    ["92.190", "92.030", "zły stan rozjazdu nr 5", "1", "60", "60", ""],
+    ["92.190", "92.280", "zły stan toru", "3", "20", "20", ""],
+    ["92.290", "93.040", "zły stan toru", "4", "30", "30", ""],
+    ["93.040", "93.050", "zły stan toru", "4", "20", "20", ""],
+    ["93.040", "93.090", "zły stan rozjazdu nr 18", "2", "60", "60", ""]
+  ],
+  "Dobrzyca Towarowa": [
+    ["7.757", "8.009", "Zły stan rozjazdów 201,203,208,272", "202", "20", "20", "* )"],
+    ["7.839", "8.014", "Zły stan głowicy rozj. 230-251,231-254", "10", "10", ""]
+  ],
+  "Dobrzyniec": [
+    ["102.992", "103.066", "tor po naprawie", "1", "40", "40", "Nowe!"]
+  ],
+  "Dobrzyniec Mącice": [
+    ["104.773", "105.491", "zły stan techniczny toru", "4", "20", "20", "* )"]
+  ],
+  "Kruszki": [
+    ["1.703", "1.802", "tor po naprawie", "1", "40", "40", "Nowe!"],
+    ["109.532", "109.642", "zły stan podkładów", "2", "80", "80", ""]
+  ],
+  "Wampirzyce": [
+    ["120.721", "120.729", "zły stan obiektu inżynieryjnego", "1,2", "60", "60", "doraźne"]
+  ],
+  "Dziewoszyce": [
+    ["74.243", "77.337", "Rozj 2 i 3 zły stan podrozjezdnic", "1", "60", "60", ""],
+    ["77.449", "77.460", "", "2", "20", "20", ""],
+    ["77.451", "77.463", "ograniczona widoczność na przejeździe", "1", "20", "20", "tylko czoło"]
+  ],
+  "Florek": [
+    ["4.475", "5.300", "Urządzenia SRK do 70 km/h", "1", "-", "70", ""]
+  ],
+  "Gostynin": [
+    ["27.445", "27.460", "warunki miejscowe przejazd", "1", "70", "70", "* )"],
+    ["28.200", "28.920", "zły stan toru", "4", "20", "20", ""],
+    ["31.651", "31.984", "ograniczona widoczność na przejeździe", "1", "60", "-", "Nowe! * )"]
+  ],
+  "Grabów": [
+    ["51.853", "53.916", "Uszkodzone części rozj. nr 4", "2", "20", "20", "* ) Nowe!"]
+  ],
+  "Grębów": [
+    ["6.028", "6.567", "układ geometryczny toru", "1", "80", "80", ""]
+  ],
+  "Hel": [
+    ["61.506", "61.638", "brak drogi hamowania", "4", "20", "-", "* )"]
+  ],
+  "Hetmanice": [
+    ["128.340", "128.580", "Tor po naprawie", "2", "80", "80", ""]
+  ],
+  "Kudowa Zdrój": [
+    ["38.058", "38.058", "ograniczona widoczność na przejeździe", "1", "20", "20", "tylko czoło"],
+    ["39.024", "39.018", "zły stan toru", "1", "20", "", ""]
+  ],
+  "Kieły": [
+    ["15.165", "15.180", "ograniczona widoczność na przejeździe", "1", "20", "20", "tylko czoło"]
+  ],
+  "Kowalewo": [
+    ["0.562", "0.723", "zły stan podrozjazdnic rozj. 25,27,28", "5", "30", "30", ""],
+    ["2.259", "2.275", "", "2", "50", "50", ""],
+    ["2.261", "2.279", "przekroczony iloczyn ruchu na przejeździe", "1", "50", "50", "tylko czoło"],
+    ["216.635", "216.678", "zły stan wiaduktu", "1", "40", "40", ""],
+    ["216.635", "216.678", "zły stan wiaduktu", "2", "40", "40", ""],
+    ["218.768", "219.926", "zły stan podkładów", "3", "20", "20", ""],
+    ["218.768", "219.926", "zły stan podkładów", "4", "20", "20", ""],
+    ["218.858", "219.740", "zły stan podtorza", "11", "20", "20", ""],
+    ["220.537", "221.177", "zły stan toru i podtorza", "1", "80", "80", ""],
+    ["220.537", "221.177", "zły stan toru i podtorza", "2", "80", "80", ""]
+  ],
+  "Kraśnica": [
+    ["102.315", "102.329", "zły stan techniczny mostu", "1,2", "40", "40", ""],
+    ["108.334", "108.341", "zły stan rozjazdu nr 200", "204", "20", "20", "zwrotny, doraźne"],
+    ["108.339", "108.481", "geometria łuku", "203", "25", "25", ""]
+  ],
+  "Kszęty": [
+    ["131.964", "131.972", "zły stan toru", "1,2", "20", "20", ""]
+  ],
+  "LCS Grodzisk Mazowiecki": [
+    ["4.707", "5.110", "brak drogi hamowania przed przejazdem km 4.702", "1", "-", "40", "przeciwny kierunek"]
+  ],
+  "LCS Kleszczów": [
+    ["165.627", "166.954", "zły stan techniczny nawierzchni", "1", "120", "120", ""],
+    ["167.904", "167.928", "obiekt mostowy nie do 160 km/h", "1", "120", "120", ""]
+  ],
+  "LCS Perzów": [
+    ["51.189", "51.637", "Zły stan toru", "11", "20", "20", ""]
+  ],
+  "LCS Żywiec": [
+    ["56.300", "56.700", "krótka krzywa - brak widoczności", "1", "30", "30", "139"],
+    ["69.343", "69.359", "zły stan podtorza", "1", "30", "30", ""],
+    ["72.320", "72.360", "zły stan przepustu km 75.458", "1", "40", "40", ""],
+    ["75.453", "75.463", "", "1", "40", "40", "wprost i zwrotny"],
+    ["85.285", "86.115", "brak widoczności przejazdy 85.411|85.831|85.988", "1", "40", "40", "Nowe!"]
+  ],
+  "Lilianowo": [
+    ["56.581", "56.700", "zły stan mostownic i widoczność", "1", "10", "10", ""]
+  ],
+  "Lisków": [
+    ["66.190", "66.976", "Zły stan toru", "6", "20", "20", "* )"],
+    ["66.248", "67.002", "Zły stan toru", "8", "20", "20", "* )"]
+  ],
+  "Łapanów": [
+    ["143.017", "143.060", "zły stan obiektu inż.", "1", "50", "50", ""],
+    ["145.754", "146.545", "zły stan podtorza", "10", "30", "30", ""],
+    ["177.455", "177.566", "zły stan mostownic", "1,2", "70", "70", ""],
+    ["182.604", "182.631", "zły stan przepustu", "1,2", "100", "100", ""]
+  ],
+  "Łąck": [
+    ["31.659", "31.984", "ograniczona widoczność", "1", "60", "-", ""],
+    ["33.591", "33.930", "ograniczona widoczność", "1", "60", "-", ""],
+    ["38.026", "38.038", "przekroczony iloczyn ruchu", "1", "50", "50", ""]
+  ],
+  "Minkowice": [
+    ["190.600", "191.750", "układ geometryczny toru", "1", "100", "100", "LK7"]
+  ],
+  "Modlinków": [
+    ["19.532", "19.548", "ograniczona widoczność na przejeździe", "1", "30", "30", "tylko czoło"],
+    ["20.524", "20.562", "zły stan toru", "1", "20", "20", ""]
+  ],
+  "Nowa Wieś Wielka": [
+    ["-1.395", "0.200", "-", "?", "75", "50", "Vd) Linia 201, nieoznakowane"],
+    ["-1.270", "0.208", "-", "?", "55", "50", "Vd) Linia 201, nieoznakowane"],
+    ["344.923", "345.100", "zły stan techniczny nawierzchni", "1", "20", "20", "131"],
+    ["344.985", "345.150", "zły stan techniczny nawierzchni", "3", "10", "10", ""],
+    ["345.079", "345.179", "układ geometryczny toru - łącznik rozj. 12 i 13", "1/2", "30", "30", "kierunek zwrotny, nieoznakowane"],
+    ["345.143", "345.177", "zły stan techniczny rozjazdu 16", "1", "20", "20", "wprost i zwrotny, nieoznakowane"],
+    ["345.851", "345.881", "zły stan techniczny rozjazdu 65", "1", "15", "10", "wprost i zwrotny, nieoznakowane"]
+  ],
+  "Odolany": [
+    ["-0.050", "0.030", "zły stan rozj. nr 47,48,52,53", "6", "10", "10", "wjazd/wyjazd 6-13 rej. Wsch."],
+    ["0.200", "0.220", "zły stan podrozjezdnic nr 83", "8", "10", "10", ""],
+    ["0.625", "0.645", "zły stan rozj. nr 68", "3", "20", "20", ""],
+    ["1.190", "1.223", "zużycie części w rozj. nr 28", "9", "10", "10", ""],
+    ["1.450", "1.500", "zły stan toru", "7", "10", "10", ""],
+    ["1.662", "", "brak dróżnika przejazdowego", "", "70", "120", "20"],
+    ["2.311", "2.551", "ograniczona widzialność przejścia 2.341", "", "70", "120", "20"],
+    ["2.518", "", "", "", "70", "", "SHP przed tarczą ToM"],
+    ["2.700", "", "SHP przed sem. R", "", "50", "", "9"],
+    ["2.867", "", "ograniczona widzialność przejazdu", "1", "20", "20", "5"],
+    ["3.400", "", "SHP przed tarczą ToR", "", "", "", ""]
+  ],
+  "Orniki": [
+    ["44.749", "44.856", "Prace przy budowie wiaduktu", "1,2", "70", "70", "Nowe"],
+    ["45.959", "45.992", "Zły stan rozjazdu nr 22", "", "30", "30", ""],
+    ["46.755", "47.933", "Zły stan toru i podtorza", "1", "30", "20", ""],
+    ["132.026", "132.042", "przekroczony iloczyn i widoczność", "1", "20", "20", "tylko czoło"]
+  ],
+  "Pszczyna": [
+    ["32.205", "32.986", "Ograniczona widoczność na przejeździe", "1,2", "120", "120", "139"],
+    ["32.986", "33.767", "Ograniczona widoczność na przejeździe", "1,2", "120", "120", ""]
+  ],
+  "Rudnik nad Sanem": [
+    ["120.000", "123.800", "zły stan toru", "2", "50", "50", "68"],
+    ["121.772", "122.294", "ograniczona widoczność", "1", "80", "-", ""],
+    ["123.445", "123.903", "ograniczona widoczność", "1", "-", "70", ""],
+    ["123.800", "124.000", "zły stan podkładów i przytwierdzenia", "2", "40", "40", ""]
+  ],
+  "Sierakówek": [
+    ["19.090", "19.420", "ograniczona widoczność", "1", "60", "-", "33"],
+    ["19.940", "20.650", "zły stan toru", "2", "20", "20", ""]
+  ],
+  "Skawce": [
+    ["37.300", "37.450", "uszkodzenie konstrukcji mostu", "1", "30", "30", ""]
+  ],
+  "Skawce Sucha Beskidzka": [
+    ["40.500", "40.750", "układ geometryczny toru", "1", "60", "60", ""],
+    ["40.750", "40.850", "niestabilność podtorza", "1", "40", "40", ""],
+    ["40.850", "41.400", "układ geometryczny toru", "1", "60", "60", ""],
+    ["41.400", "42.000", "", "1", "50", "50", ""]
+  ],
+  "Skrzynki": [
+    ["47.800", "48.580", "zły stan toru", "4", "20", "20", "25"]
+  ],
+  "Stefanowo": [
+    ["35", "", "", "", "", "", ""],
+    ["61.956", "62.231", "ograniczona widoczność", "1", "50", "", "tylko czoło"]
+  ],
+  "Strzelce Kujawskie": [
+    ["13.545", "14.283", "zły stan toru i podkładów", "3", "20", "20", "33"]
+  ],
+  "Sucha Beskidzka": [
+    ["-0.480", "0.300", "zły stan techniczny toru", "2", "30", "30", "98"],
+    ["1.067", "", "Przejazd - brak widoczności", "", "30", "30", ""],
+    ["45.191", "", "Przejazd - brak widoczności", "", "30", "30", "97"],
+    ["51.700", "51.750", "Zły stan toru w przejeździe", "", "30", "30", "autobusy szynowe"]
+  ],
+  "Suszec": [
+    ["17.600", "19.000", "Nierówność pionowa na szkodach górniczych", "1", "30", "30", "148"]
+  ],
+  "Tarkowice": [
+    ["112.461", "112.537", "realizacja inwestycji", "1,2", "60", "60", ""]
+  ],
+  "Trzymałkowice": [
+    ["63.135", "63.148", "Zły stan wiaduktu", "1", "40", "40", ""]
+  ],
+  "Worszowice - Strumień": [
+    ["4.532", "5.052", "ograniczona widoczność", "1,2", "80", "80", "* )"]
+  ],
+  "Wełtawa": [
+    ["62.579", "62.612", "Zły stan rozjazdu", "2", "50", "50", ""],
+    ["62.672", "63.115", "Zły stan toru", "6", "30", "30", ""],
+    ["63.667", "63.713", "Zły stan rozjazdu", "1", "60", "60", ""],
+    ["65.033", "65.456", "Zły stan mostownic", "1", "40", "40", ""]
+  ],
+  "Wijewo": [
+    ["42.833", "43.497", "Tor po naprawie", "1,2", "30", "30", "doraźne"],
+    ["44.798", "45.097", "niestabilne podtorze", "1", "80", "80", ""],
+    ["44.798", "45.097", "niestabilne podtorze", "2", "80", "80", ""],
+    ["45.600", "45.635", "zły stan rozjazdu nr 31", "5", "50", "50", ""],
+    ["45.677", "45.781", "zły stan rozjazdu 8", "4", "20", "20", ""],
+    ["45.832", "46.571", "zły stan toru", "6", "20", "20", "doraźne"],
+    ["46.441", "46.454", "waga wagonowa", "10", "10", "10", ""],
+    ["46.644", "46.710", "zły stan rozjazdu krzyżowego 24", "2", "70", "70", "Nowe!"],
+    ["46.676", "46.781", "niestabilne podtorze", "6", "60", "60", "doraźne"],
+    ["48.503", "48.516", "Przekroczony iloczyn ruchu", "1", "50", "50", "tylko czoło * )"]
+  ],
+  "Wronowice": [
+    ["73.376", "73.581", "Zły stan krawędzi peronu", "1", "60", "60", ""],
+    ["75.034", "76.077", "zły stan toru", "4", "20", "20", ""],
+    ["76.845", "76.945", "zły stan toru", "1", "40", "40", ""]
+  ],
+  "Zakopane": [
+    ["41.000", "41.065", "przejazdy brak widoczności + zły stan mostu km 41.042", "1", "30", "30", ""],
+    ["41.807", "43.200", "R=295,245,214,238,303 m brak kp", "2", "140", "40", ""]
+  ],
+  "Zawadówka": [
+    ["237.490", "237.720", "układ geometryczny toru", "1", "80", "80", "7"],
+    ["237.490", "237.720", "układ geometryczny toru", "2", "80", "80", ""]
+  ],
+  "Zielona Góra Główna": [
+    ["0.000", "0.800", "", "-", "46", "60", "T 370"],
+    ["0.000", "0.800", "", "-", "47", "70", "370"],
+    ["0.900", "0.960", "układ geometryczny toru", "4", "40", "40", "370"],
+    ["", "", "Wjazd na tor zakończony kozłem", "8", "-", "20", ""],
+    ["153.660", "153.930", "układ geometryczny toru", "1", "80", "80", "273"],
+    ["154.510", "154.810", "układ geometryczny toru", "1", "80", "80", "273 na stacji"]
+  ]
+}
